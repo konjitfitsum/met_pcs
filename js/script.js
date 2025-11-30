6f6e19a4a0f905d8299e5331d7d3ceae1bfc93f1
Fitsum <166730412+konjitfitsum@users.noreply.github.com>
2025-11-30 20:53:14 +0000

    black and white toggle added

--- js/script.js
+++ js/script.js
@@ -24,3 +24,22 @@ setInterval(() => {
   index = (index + 1) % images.length;
   showSlide(index);
 }, 4000);
+
+const toggle = document.getElementById("bwToggle");
+
+// Load saved state
+if (localStorage.getItem("bwMode") === "on") {
+  document.body.classList.add("bw-mode");
+  toggle.checked = true;
+}
+
+// Toggle & save
+toggle.addEventListener("change", function() {
+  if (this.checked) {
+    document.body.classList.add("bw-mode");
+    localStorage.setItem("bwMode", "on");
+  } else {
+    document.body.classList.remove("bw-mode");
+    localStorage.setItem("bwMode", "off");
+  }
+});
